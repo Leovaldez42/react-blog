fd993cc5ca1ec565f9dc1fc484b4c5c793858689
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-10-16 23:25:30 +0530

    added create project and basic redux

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import SignIn from './components/auth/SignIn';
 import SignUp from './components/auth/Signup';
 import Dashboard from './components/dashboard/Dashboard';
 import Navbar from './components/layout/Navbar';
+import CreateProject from './components/project/CreateProject';
 import ProjectDetails from './components/project/ProjectDetails';
 
 class App extends Component{
@@ -13,10 +14,11 @@ class App extends Component{
         <div className="App">
           <Navbar />
           <Switch>
-            <Route exact path='/' component={Dashboard} />
-            <Route path='/project/:id' component={ProjectDetails} />
+            <Route exact path="/" component={Dashboard} />
+            <Route path="/project/:id" component={ProjectDetails} />
             <Route path="/signin" component={SignIn} />
             <Route path="/signup" component={SignUp} />
+            <Route path="/create" component={CreateProject} />
           </Switch>
         </div>
       </Router>
