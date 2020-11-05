831714f519aa46c5254addc2e73d2c76071d537b
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-11-05 23:42:51 +0530

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {createStore, applyMiddleware, compose} from 'redux'
-import rootReducer from './store/reducers/rootReducer';
-import {Provider} from 'react-redux'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './store/reducers/rootReducer'
+import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
-import { reduxFirestore, getFirestore } from 'redux-firestore'
-import { reactReduxFirebase, getFirebase } from 'react-redux-firebase'
+import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore'
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase'
 import fbConfig from './config/fbConfig'
+import firebase from 'firebase/app'
 
 const store = createStore(rootReducer, 
-    compose(
-        applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
-        reduxFirestore(fbConfig),
-        reactReduxFirebase(fbConfig)
-    )
-);
+    compose(applyMiddleware(thunk.withExtraArgument({getFirestore, getFirebase})), 
+    reduxFirestore(fbConfig)))
 
-ReactDOM.render(<Provider store={store}>  <App />   </Provider>,document.getElementById('blog'));
+const rrfProps = {firebase, config: fbConfig, dispatch: store.dispatch, createFirestoreInstance}
+
+ReactDOM.render(<Provider store={store}>
+    <ReactReduxFirebaseProvider{...rrfProps}>
+        <App />
+    </ReactReduxFirebaseProvider>
+</Provider>, document.getElementById("blog"));
+
 serviceWorker.unregister();
