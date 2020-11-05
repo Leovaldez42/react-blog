5bc860cffcdf265f5589bd3824db33d45cc576b7
Leovaldez42 <ghsharma_b18@et.vjti.ac.in>
2020-11-05 23:51:35 +0530

    we can add data to firebase now

--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,9 +9,13 @@ const initialState = {
 const projectReducer = (state = initialState, action) => {
     switch(action.type) {
         case 'CREATE_PROJECT':
-            console.log('created project', action.project)
+            console.log('created project', action.project);
+            return state
+        case 'CREATE_PROJECT_ERROR':
+            console.log('create project error', action.err);
+        default:
+            return state;
     }
-    return state
 }
 
 export default projectReducer
